3bc3467d8560575adebcfd5ea9335fe0f7e50ed8
stevecyj <ycsteven.tsao@gmail.com>
2022-07-14 09:17:13 +0800

    vite-plugin-pages,vite-plugin-vue-layouts

--- vite.config.js
+++ vite.config.js
@@ -1,34 +1,38 @@
-import { fileURLToPath, URL } from "url";
-import path from "path";
+import { fileURLToPath, URL } from 'url';
+import path from 'path';
 
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
-import AutoImport from "unplugin-auto-import/vite";
-import Components from "unplugin-vue-components/vite";
-import { createSvgIconsPlugin } from "vite-plugin-svg-icons";
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import AutoImport from 'unplugin-auto-import/vite';
+import Components from 'unplugin-vue-components/vite';
+import { createSvgIconsPlugin } from 'vite-plugin-svg-icons';
+import Pages from 'vite-plugin-pages';
+import Layouts from 'vite-plugin-vue-layouts';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     AutoImport({
-      imports: ["vue", "vue-router", "vue-i18n"],
-      dts: "src/auto-imports.js",
+      imports: ['vue', 'vue-router', 'vue-i18n'],
+      dts: 'src/auto-imports.js',
     }),
     Components({
       // 從 `./src/components/` 路徑查找
-      extensions: ["vue"],
+      extensions: ['vue'],
       include: [/\.vue$/, /\.vue\?vue/],
-      dts: "src/auto-components.js",
+      dts: 'src/auto-components.js',
     }),
     createSvgIconsPlugin({
-      iconDirs: [path.resolve(process.cwd(), "src/assets/svg")],
-      symbolId: "[dir]/[name]",
+      iconDirs: [path.resolve(process.cwd(), 'src/assets/svg')],
+      symbolId: '[dir]/[name]',
     }),
+    Pages(),
+    Layouts(),
   ],
   resolve: {
     alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
 });
