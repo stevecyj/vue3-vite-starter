0996dd9293322dba6e79618d3f257804cad7f026
stevecyj <ycsteven.tsao@gmail.com>
2022-07-14 09:56:17 +0800

    pinia,get data from store

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,7 +1,11 @@
-<script setup></script>
+<script setup>
+import { useCounterStore } from '@/stores/counter.js';
+const store = useCounterStore();
+</script>
 
 <template>
   <main>
     <TheWelcome />
+    <h1>{{ store.name }}</h1>
   </main>
 </template>
